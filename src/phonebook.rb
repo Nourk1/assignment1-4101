f9f48c58d5ec7ad1ee8c03b1ac2cfcf4ce938fc1
Nourk1 <137818611+Nourk1@users.noreply.github.com>
2025-09-16 21:38:03 -0500

    Update phonebook.rb
    phonebook

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,42 @@
 class PhoneBook
     def initialize
+        # Store contacts here 
+        @contacts = {}
+        # Store listed numbers 
+        @public_numbers = {}
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        return false if @contacts.key?(name)
+
+    
+        return false unless number =~ /^\d{3}-\d{3}-\d{4}$/
+
+    
+        if is_listed && @public_numbers.key?(number)
+            return false
+        end
+
+    
+        @contacts[name] = { number: number, listed: is_listed }
+        @public_numbers[number] = name if is_listed
+        true
+
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        entry = @contacts[name]
+        return nil unless entry && entry[:listed]
+        entry[:number]
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        name = @public_numbers[number]
+        name ? name : nil
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        @contacts.select { |_, entry| entry[:number].start_with?(areacode) }
+            .keys
     end
 end
